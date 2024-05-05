a01011b907658aa95c03fc955401f674356108fc
woruka onward <worukaonward@gmail.com>
2024-05-05 19:29:24 +0100

    commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const navMenu = document.querySelector('.nav-bar')
+const hamBuregerMenu = document.querySelector('.fa-bars')
+const closeMenu = document.querySelector('.fa-times')
+
+
+hamBuregerMenu.addEventListener('click', () =>)
+
+
+
+
+
+
+
+
+
+
+
+
+
+AOS.init()
