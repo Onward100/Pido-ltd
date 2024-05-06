bb127aa95cf93b600db3c06812d3c73d90945615
woruka onward <worukaonward@gmail.com>
2024-05-06 14:06:25 +0100

    commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const navMenu = document.querySelector(".nav-bar");
 const hamBuregerMenu = document.querySelector(".fa-bars");
 const closeMenu = document.querySelector(".fa-times");
-const navLinks = document.querySelectorAll(".nav-links")
+const navLinks = document.querySelectorAll(".nav-links");
 
 hamBuregerMenu.addEventListener("click", () => {
   navMenu.style.transform = "translateY(0)";
@@ -10,11 +10,14 @@ closeMenu.addEventListener("click", () => {
   navMenu.style.transform = "translateY(-2000px)";
 });
 
-navLinks.forEach(link => {
-  link.addEventListener("click", () =>{
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
     navMenu.style.transform = "translateY(-2000px)";
-  })
+  });
 });
 
-
-
+// document.body.addEventListener("click", (e) => {
+//   if (!navMenu.contains(e.target)) {
+//     navMenu.style.transform = "translateY(-2000px)";
+//   }
+// });
