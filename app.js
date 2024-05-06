e72c08d3e87982630d2b8c2b2fefcdf91e71da26
woruka onward <worukaonward@gmail.com>
2024-05-06 05:38:53 +0100

    commit message

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const navMenu = document.querySelector('.nav-bar')
-const hamBuregerMenu = document.querySelector('.fa-bars')
-const closeMenu = document.querySelector('.fa-times')
+const navMenu = document.querySelector(".nav-bar");
+const hamBuregerMenu = document.querySelector(".fa-bars");
+const closeMenu = document.querySelector(".fa-times");
+const navLinks = document.querySelectorAll(".nav-links")
+
+hamBuregerMenu.addEventListener("click", () => {
+  navMenu.style.transform = "translateY(0)";
+});
+closeMenu.addEventListener("click", () => {
+  navMenu.style.transform = "translateY(-2000px)";
+});
+
+navLinks.forEach(link => {
+  link.addEventListener("click", () =>{
+    navMenu.style.transform = "translateY(-2000px)";
+  })
+});
 
 
-hamBuregerMenu.addEventListener('click', () =>)
 
-
-
-
-
-
-
-
-
-
-
-
-
-AOS.init()
